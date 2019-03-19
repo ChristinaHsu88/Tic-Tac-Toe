687faf3fcfb5dbb7fec7673459fa7e8ea8807ae4
ChristinaHsu88 <christina.hsu.ca@gmail.com>
2019-03-19 13:06:54 -0700

    add lines in Game component to have it track the progress & remove repetitive ones in Board component

--- src/index.js
+++ src/index.js
@@ -27,16 +27,19 @@ function Square(props) {
 
 
 class Board extends React.Component {
-  constructor(props) {
-  super(props)
-  this.state = {
-    squares: Array(9).fill(null),  /* set the initial state as null and be changed later when input is entered */
-    xIsNext: true
-    }
-  }
+  // constructor(props) { /* replaced with the constructor in Game to store data */
+  // super(props)
+  // this.state = {
+  //   squares: Array(9).fill(null),  /* set the initial state as null and be changed later when input is entered */
+  //   xIsNext: true
+  //   }
+  // }
 
   handleClick(i) {
     const squares = this.state.squares.slice() /* make a copy of the array to modify the data instead of modifying it directly to the original array */
+    if (calculateWinner(squares) || squares[i]) {
+      return
+    }
     squares[i] = this.state.xIsNext ? 'X' : 'O'
     this.setState({
       squares: squares,
@@ -47,13 +50,20 @@ class Board extends React.Component {
   renderSquare(i) {
     return (
       <Square 
-        value={this.state.squares[i]}
-        onClick={() => this.handleClick(i)}
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
         />) /* passing value and onClick props to Square component */
   }
-  
+
   render() {
-    const status = 'Next player: X'
+    /* remove below as now Game component keeps track of the status and state */
+    // const winner = calculateWinner(this.state.squares)
+    // let status
+    // if (winner) {
+    //   status = 'Winner: ' + winner
+    // } else {
+    //   status = 'Next player: ' + (this.state.xIsNext? 'X' : 'O')
+    // }
 
     return (
       <div>
@@ -79,7 +89,25 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {   /* store the history data */
+      history: [{
+        squares: Array(9).fill(null)
+      }],
+      xIsNext: true
+    }
+  }
   render() {
+    const history = this.state.history
+    const current = history[history.length - 1]
+    const winner = calculateWinner(current.squares)
+    let status
+    if (winner) {
+      status = 'Winner: ' + winner
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O')
+    }
     return (
       <div className="game">
         <div className="game-board">
@@ -92,6 +120,27 @@ class Game extends React.Component {
       </div>
     )
   }
+}
+
+/* situation where a winner will appear */
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ]
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i]
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) { /* square[a] 'X' or 'O' is the same player as in square[b] and square[c] */
+      return squares[a]
+    }
+  }
+  return null
 }
 
 ReactDOM.render(
