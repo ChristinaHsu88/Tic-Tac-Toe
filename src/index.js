22cba4071c3b2c77dd150d0a117439959575ce13
ChristinaHsu88 <christina.hsu.ca@gmail.com>
2019-03-19 15:00:35 -0700

    implement time travel so the user can go back to previous step

--- src/index.js
+++ src/index.js
@@ -82,12 +82,13 @@ class Game extends React.Component {
       history: [{
         squares: Array(9).fill(null)
       }],
+      stepNumber: 0, /* indicating which step we're currently viewing */
       xIsNext: true
     }
   }
 
   handleClick(i) {
-    const history = this.state.history
+    const history = this.state.history.slice(0, this.state.stepNumber + 1)
     const current = history[history.length - 1]
     const squares = current.squares.slice() /* make a copy of the array to modify the data instead of modifying it directly to the original array */
     if (calculateWinner(squares) || squares[i]) {
@@ -98,13 +99,21 @@ class Game extends React.Component {
       history: history.concat([{   /* use concat instead of push to avoid mutating the original array */
         squares: squares
       }]),
+      stepNumber: history.length,
       xIsNext: !this.state.xIsNext /* tracking the changing state of xIsNext */
+    })
+  }
+
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0
     })
   }
 
   render() {
     const history = this.state.history
-    const current = history[history.length - 1]
+    const current = history[this.state.stepNumber]
     const winner = calculateWinner(current.squares)
 
     const moves = history.map((step, move) => {
@@ -112,7 +121,7 @@ class Game extends React.Component {
         'Go to move #' + move :
         'Go to game start'
       return (
-        <li>
+        <li key={move}>
           <button onClick={() => this.jumpTo(move)}>{desc}</button>
         </li>
       )
